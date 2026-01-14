d0a65cecef74015ca7da3daf3be220a3d6402c3d
Frank Guerrero <frankmayorgaguerrero@gmail.com>
2026-01-13 20:31:03 -0500

    Se organiza los archivos, para mejorar la estructura

--- src/main/java/com/fguerrero/ticket_service/repository/UserRepository.java
+++ src/main/java/com/fguerrero/ticket_service/repository/UserRepository.java
@@ -1,5 +1,6 @@
-package com.fguerrero.ticket_service;
+package com.fguerrero.ticket_service.repository;
 
+import com.fguerrero.ticket_service.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
